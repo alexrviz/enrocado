3059696f59a785271a545ef9c846cbdcca224179
alexrviz <alejanjim.14.2001@gmail.com>
2026-08-11 17:42:44 +0200

    Publicar config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Sustituye esta URL después de desplegar el Worker.
+// Déjala vacía para probar el diseño sin enviar datos.
+window.SITE_CONFIG = {
+  formEndpoint: "https://api.enrocado.com/subscribe",
+};
